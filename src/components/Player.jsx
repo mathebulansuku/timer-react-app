9d0702e729ea2101684caae26ee6a35dd2aecc01
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-15 12:01:27 +0200

    state of playerName

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState(null);
+  const [submitName, setSubmitName] = useState(false);
+
+  function handleClick(name) {
+    submitName(true);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitName ? playerName : "Unknown Entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
