a183feae2ce70c458825883c7c85754bc41e495e
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-15 14:22:55 +0200

    timer component added

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+let timer;
+
+export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timer = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer);
+  }
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired ? <p>You lost</p> : null}
+      <p className="challenge-time">{targetTime} seconds</p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
+    </section>
+  );
+}
