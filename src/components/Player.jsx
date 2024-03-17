be2771272172943c864d6d8ccd941f117fe8ef38
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-17 15:37:38 +0200

    useRef on Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,12 +4,6 @@ export default function Player() {
   const inputName = useRef();
 
   const [playerName, setPlayerName] = useState("");
-  // const [submitName, setSubmitName] = useState(false);
-
-  // function handleChange(event) {
-  //   setSubmitName(false);
-  //   setPlayerName(event.target.value);
-  // }
 
   function handleClick() {
     setPlayerName(inputName.current.value);
@@ -19,12 +13,7 @@ export default function Player() {
     <section id="player">
       <h2>Welcome {playerName ? playerName : "Unknown Entity"}</h2>
       <p>
-        <input
-          ref={inputName}
-          type="text"
-          // onChange={handleChange}
-          // value={playerName}
-        />
+        <input ref={inputName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
